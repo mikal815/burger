5be70767ad88b524359f08971154139663aa11ff
Mike M <mikal815@users.noreply.github.com>
2018-08-18 01:47:14 -0400

    server

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+var connection = require('./connection.js');
+
+var orm = {
+
+	selectAll : function(table, cb){
+		connection.query("SELECT * FROM ??",[table], function(err, result) {
+			if (err) throw err;
+			cb(result); //once query has successfully completed, return the result with our callback
+		});
+	},
+
+	insertOne : function(table, column, value, cb){
+		
+		connection.query("INSERT INTO ?? (??) VALUES (?)", [table, column, value], function(err, result) {
+			if(err) throw err;
+			cb(result);
+		});
+	},
+
+	updateOne: function(table, columnAlter, newValue, columnQuery, valueQuery, cb){
+
+		connection.query("UPDATE ?? SET ?? = ? WHERE ?? = ?", [table, columnAlter, newValue, columnQuery, valueQuery], function(err, result) {
+			if(err) throw err;
+			cb(result);
+		});
+	},
+
+	deleteOne: function(table, columnQuery, valueQuery, cb){
+		connection.query("DELETE FROM ?? WHERE ?? = ?", [table, columnQuery, valueQuery], function(err, result) {
+			if(err) throw err;
+			cb(result);
+		});
+	}
+}
+
+
+module.exports = orm;
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false })); //Inteprets client POST data "false" only allows strings or arrays to be passed
+
+
+var hbs = exphbs.create({ //creates instance of handlebars which can be used globally
+    helpers: {
+        inc: function (index) { 
+              return parseInt(index) + 1; 
+         }
+    },
+    defaultLayout: 'main'
+    });
+
+app.engine('handlebars', hbs.engine) // uses the above handlebars instance
+app.set('view engine', 'handlebars'); //set view engine to handlebars
+
+app.use(express.static(process.cwd() + "/public")); //Targets current working directory
+
+app.use(methodOverride("_method")); //package that looks for the text "?_method="*" appended to my POST request action from view.  *(can be PUT or DELETE)
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes); 
+
+app.listen(port, function() {
+    console.log(`App listening on port ${port}`);
+});
